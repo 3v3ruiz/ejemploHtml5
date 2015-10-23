cb9e316aafd95dc324aaa76739f3096e94e521e6
inftel08 <inftel08@192.168.183.43>
2015-10-23 20:16:58 +0200

    Merge origin/master
    Conflicts:
    	public_html/css/cenas.css

--- public_html/js/recetas_texto.js
+++ public_html/js/recetas_texto.js
@@ -1,13 +1,3 @@
-/*********************************************************************
-*  #### Twitter Post Fetcher v13.1 ####
-*  Coded by Jason Mayes 2015. A present to all the developers out there.
-*  www.jasonmayes.com
-*  Please keep this disclaimer with my code if you use it. Thanks. :-)
-*  Got feedback or questions, ask here:
-*  http://www.jasonmayes.com/projects/twitterApi/
-*  Github: https://github.com/jasonmayes/Twitter-Post-Fetcher
-*  Updates will be posted to this site.
-*********************************************************************/
 (function(w,p){"function"===typeof define&&define.amd?define([],p):"object"===typeof exports?module.exports=p():p()})(this,function(){function w(a){return a.replace(/<b[^>]*>(.*?)<\/b>/gi,function(a,g){return g}).replace(/class=".*?"|data-query-source=".*?"|dir=".*?"|rel=".*?"/gi,"")}function p(a){a=a.getElementsByTagName("a");for(var c=a.length-1;0<=c;c--)a[c].setAttribute("target","_blank")}function n(a,c){for(var g=[],f=new RegExp("(^| )"+c+"( |$)"),h=a.getElementsByTagName("*"),b=0,k=h.length;b<
 k;b++)f.test(h[b].className)&&g.push(h[b]);return g}var B="",k=20,C=!0,u=[],x=!1,v=!0,q=!0,y=null,z=!0,D=!0,A=null,E=!0,F=!1,r=!0,G=!0,m=null,H={fetch:function(a){void 0===a.maxTweets&&(a.maxTweets=20);void 0===a.enableLinks&&(a.enableLinks=!0);void 0===a.showUser&&(a.showUser=!0);void 0===a.showTime&&(a.showTime=!0);void 0===a.dateFunction&&(a.dateFunction="default");void 0===a.showRetweet&&(a.showRetweet=!0);void 0===a.customCallback&&(a.customCallback=null);void 0===a.showInteraction&&(a.showInteraction=
 !0);void 0===a.showImages&&(a.showImages=!1);void 0===a.linksInNewWindow&&(a.linksInNewWindow=!0);void 0===a.showPermalinks&&(a.showPermalinks=!0);if(x)u.push(a);else{x=!0;B=a.domId;k=a.maxTweets;C=a.enableLinks;q=a.showUser;v=a.showTime;D=a.showRetweet;y=a.dateFunction;A=a.customCallback;E=a.showInteraction;F=a.showImages;r=a.linksInNewWindow;G=a.showPermalinks;var c=document.getElementsByTagName("head")[0];null!==m&&c.removeChild(m);m=document.createElement("script");m.type="text/javascript";m.src=
@@ -19,106 +9,7 @@ d+('<p class="timePosted">'+f[b].getAttribute("aria-label")+"</p>"))):a[b].inner
 (r?' target="_blank">':">")+'Reply</a><a href="https://twitter.com/intent/retweet?tweet_id='+m[b]+'" class="twitter_retweet_icon"'+(r?' target="_blank">':">")+'Retweet</a><a href="https://twitter.com/intent/favorite?tweet_id='+m[b]+'" class="twitter_fav_icon"'+(r?' target="_blank">':">")+"Favorite</a></p>");F&&void 0!==h[b]&&(l=h[b],void 0!==l?(l=l.innerHTML.match(/data-srcset="([A-z0-9%_\.-]+)/i)[0],l=decodeURIComponent(l).split('"')[1]):l=void 0,d+='<div class="media"><img src="'+l+'" alt="Image from tweet" /></div>');
 c.push(d);b++}if(null===A){a=c.length;g=0;f=document.getElementById(B);for(h="<ul>";g<a;)h+="<li>"+c[g]+"</li>",g++;f.innerHTML=h+"</ul>"}else A(c);x=!1;0<u.length&&(H.fetch(u[0]),u.splice(0,1))}};return window.twitterFetcher=H});
 
-var YT = 'undefined';
 
-
-function showResponse(response) {
-    YT = response;
-    //alert(YT.items[0].id.videoId);
-    
-    var cab = "https://www.youtube.com/embed/";
-    
-    //alert (YT.items[2].snippet[0].description.videoId);
-    
-    for (var i = 1; i<=4; i++) {
-        var res = cab.concat(YT.items[i-1].id.videoId);
-        var num_video = "video";
-        var elem = num_video.concat(i.toString());
-        document.getElementById(elem.toString()).src=res; 
-    }
-      
-}
-
-function search(prueba) {
-    
-    if (prueba!="No Tweet"){ 
-       
-        gapi.client.setApiKey('AIzaSyCWzGO9Vo1eYOW4R4ooPdoFLmNk6zkc0Jw');
-        gapi.client.load('youtube', 'v3', function() {
-            qVar = prueba;
-
-            var request = gapi.client.youtube.search.list({
-                type: 'video',
-                part: 'id',
-                q: qVar
-            },
-            {
-                type: 'video',
-                part: 'snippet',
-                q: qVar
-
-            });
-
-            request.execute(onSearchResponse);
-
-        });
-    }
-}
-
-function onSearchResponse(response) {
-    showResponse(response);
-}
-
-//Definicion de varios var para ultimas busquedas
-var config1 = {
-  "id": '657499598894845952',
-  "domId": 'example5',
-  "maxTweets": 5,
-  "enableLinks": false,
-  "showUser": false,
-  "showTime": false,
-  "dateFunction": '',
-  "showRetweet": false,
-  "customCallback": handleTweetN1,
-  "showInteraction": false
-};
-var config2 = {
-  "id": '657499598894845952',
-  "domId": 'example5',
-  "maxTweets": 5,
-  "enableLinks": false,
-  "showUser": false,
-  "showTime": false,
-  "dateFunction": '',
-  "showRetweet": false,
-  "customCallback": handleTweetN2,
-  "showInteraction": false
-};
-var config3 = {
-  "id": '657499598894845952',
-  "domId": 'example5',
-  "maxTweets": 5,
-  "enableLinks": false,
-  "showUser": false,
-  "showTime": false,
-  "dateFunction": '',
-  "showRetweet": false,
-  "customCallback": handleTweetN3,
-  "showInteraction": false
-};
-var config4 = {
-  "id": '657499598894845952',
-  "domId": 'example5',
-  "maxTweets": 5,
-  "enableLinks": false,
-  "showUser": false,
-  "showTime": false,
-  "dateFunction": '',
-  "showRetweet": false,
-  "customCallback": handleTweetN4,
-  "showInteraction": false
-};
-//FIN DEFINICIONES
 var config5 = {
   "id": '657499598894845952',
   "domId": 'example5',
@@ -132,60 +23,44 @@ var config5 = {
   "showInteraction": false
 };
 
-
-
+var search;
+var busqueda;
+    
 function handleTweets(tweets){
+    
+    var key = extraer_keyword(tweets[0]);  
 
-    var key = extraer_keyword(tweets[0]);
-    search(key);
-}
+    $.getJSON("JSON/Recetas.json",function (json){
+        
+        var pos;
+        var salir = false;
+        var x = 0;
+        
+        while (!salir && x<json.Results.length) {
+            if (json.Results[x].Title == key) {
+                salir=true;
+                pos = x;
+            } else {
+                x++;
+            }
 
-function handleTweetN1(tweets){
-
-    var key = extraer_keyword(tweets[1]);
-    search(key);
-}
-function handleTweetN2(tweets){
-
-    var key = extraer_keyword(tweets[2]);
-    search(key);
-}
-
-function handleTweetN3(tweets){
-
-    var key = extraer_keyword(tweets[3]);
-    search(key);
-}
-function handleTweetN4(tweets){
-
-    var key = extraer_keyword(tweets[4]);
-    search(key);
-}
+        }
 
 
+        $("#Titulo").html(json.Results[pos].Title);
+        $(".result").html(json.Results[pos].Receta);
+        $(".ingredientes").html(json.Results[pos].Ingredientes);
 
-function devuelvekeyword() {
-    return keyword;
+    });   
 }
 
 
 function extraer_keyword (miTweet) {
     
     var res = miTweet.split("#comidasana ");
-    //alert(miTweet.length);
-    if (res[0].length==miTweet.length){
-        document.getElementById("titulo-receta").innerHTML = "Este tweet no es una receta";
-        key="No Tweet";
-    }
-    else{
     res=res[1].split(".");
-    var receta = "Recetas de ";
-    var key = receta.concat(res[0]);
-    //document.getElementById("titulo-receta").innerHTML = "Este tweet no es una receta";
-    document.getElementById("titulo-receta").innerHTML = key;
-    
-    
-    }
-    return key;
-}
+    res=res[0].split("<");
 
+    return res[0];
+}
+    
